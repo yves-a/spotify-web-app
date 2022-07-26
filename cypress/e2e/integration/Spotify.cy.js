d00e91912003d89ec2c3e2aea61e1d02afe424e3
Yves <yalikalfic@yahoo.com>
2022-07-26 15:34:23 -0400

    changed css styles and added icon

--- cypress/e2e/integration/Spotify.cy.js
+++ cypress/e2e/integration/Spotify.cy.js
@@ -1,6 +1,6 @@
 describe('Spotify', function () {
   it('front page can be opened', function () {
     cy.visit('http://localhost:3000')
-    cy.contains('Spotify Web App')
+    cy.contains('Reminiscing Record Player')
   })
 })
